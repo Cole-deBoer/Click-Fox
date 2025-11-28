18adade9a02f6b4fbf1d3b7a53d330d84047ffe3
Cole deBoer <deboercole06@gmail.com>
2025-11-27 17:05:30 -0700

    ADDED: logo  UPDATED: nav bar to use new logo + tab bar displays logo aswell.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,11 +6,11 @@ import { AuthContext } from '../Context/AuthContext';
 // svg images
 import crown from "../Assets/crown.svg"
 import mouse from "../Assets/mouse.svg"
+import clickFoxLogo from "../Assets/click-fox-logo.png"
 
 const Elements = [
-    {Display: <strong className='text-md md:text-2xl'>Click-Fox</strong>, Route: '/'},
-    {Display: <img className='h-1/2 md:h-2/3 scale-75 md:scale-90' src={mouse} alt="click logo" />, Route: '/'},
-    //{Display: <img className='h-1/2 md:h-2/3 scale-75 md:scale-90' src={crown} alt="leaderboard" />, Route: '/leaderboard'},
+    {Display: <img className='h-1/2 md:h-3/4 scale-100 md:scale-75' src={mouse} alt="mouse icon" />, Route: '/'},
+    // {Display: <img className='h-1/2 md:h-3/4 scale-100 md:scale-75' src={crown} alt="leaderboard" />, Route: '/leaderboard'},
 ]
 
 const Navbar = () => {
@@ -29,9 +29,17 @@ const Navbar = () => {
         <div className="w-full md:w-5/6 h-12 mx-auto mt-8 md:mt-12">
             <div className="h-full flex justify-between md:scale-100">
                 {/* Left Justified Items*/}
-                <nav className="mx-4 flex gap-4 md:gap-6">
+                <nav className="flex gap-4 md:gap-1">
+
+                    {/* Logo with text */}
+                    <div className="h-8 md:h-10 mx-2 flex space-x-1 select-none cursor-pointer" onClick={() => navigate('/')}>
+                        <img src={clickFoxLogo} alt="click-fox logo" className="bg-blend-difference"/> 
+                        <strong className="text-xl md:text-3xl">click-fox</strong>
+                    </div>
+
+                    {/* other left alligned elements */}
                     {Elements.map((element, key) => (
-                        <Link key={key} to={element.Route}>
+                        <Link key={key} to={element.Route} className="gap-4">
                             <Button content={
                                 <>
                                     {element.Display}
